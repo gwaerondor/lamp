114ac8168a9729d822abb94915f695d9f0678e09
Vincent Li <vincent.x.li@ericsson.com>
2016-10-27 13:35:44 +0000

    First argument to obtain nth record

--- get_temperature.bash
+++ get_temperature.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
-TEMP=$(w3m -dump http://www.yr.no/place/Sweden/Stockholm/Stockholm/forecast.xml | grep temperature | head -n 1 | sed -e 's/^.*value.*"\(.*\)".*$/\1/g')
+LINE=1
+if [ ! -z "$1" ];
+then LINE=$1
+fi
+TEMP=$(w3m -dump http://www.yr.no/place/Sweden/Stockholm/Stockholm/forecast.xml | grep temperature | head -n $LINE | tail -1 | sed -e 's/^.*value.*"\(.*\)".*$/\1/g')
 echo ${TEMP}
