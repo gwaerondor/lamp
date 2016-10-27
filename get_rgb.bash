78c94fdc0a6ba7674efdf4e735b5710627f3e62d
Vincent Li <vincent.x.li@ericsson.com>
2016-10-27 18:03:09 +0000

    rename get_stockholm_tmp and adjust rgb value

--- get_rgb.bash
+++ get_rgb.bash
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 
-let "TEMP = $1 + 20"
-if (( $TEMP > 50 )); then
-	let "TEMP = 50"
+let "TEMP = $1 + 10"
+if (( $TEMP > 40 )); then
+	let "TEMP = 40"
 fi
 if (( $TEMP < 0 )); then
         let "TEMP = 0"
 fi
 
-let "BLUE = 255 - ( $TEMP % 10 ) * 25"
-let "RED = ( $TEMP / 10 ) * 50"
+let "RED = $TEMP * 5"
+let "BLUE = 255 - $RED"
+if (( $RED > $BLUE )); then
+	let "GREEN = $BLUE"
+else
+	let "GREEN = $RED"
+fi
+
 printf -v HEX_BLUE "%02x" "$BLUE"
 printf -v HEX_RED "%02x" "$RED"
+printf -v HEX_GREEN "%02x" "$GREEN"
 
 #echo "tmp" $TEMP
-echo $HEX_RED"00"$HEX_BLUE
+echo $HEX_RED$HEX_GREEN$HEX_BLUE
