a80b919f5e9d9f15ec8743c3da8f5ec65b9995c8
Vincent Li X <vincent.x.li@ericsson.com>
2016-10-28 11:29:28 +0200

    get_temperature.bash second argument as city

--- get_temperature.bash
+++ get_temperature.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
 LINE=1
+CITY=Stockholm
 if [ ! -z "$1" ];
 then LINE=$1
 fi
-TEMP=$(w3m -dump http://www.yr.no/place/Sweden/Stockholm/Stockholm/forecast.xml | grep temperature | head -n $LINE | tail -1 | sed -e 's/^.*value.*"\(.*\)".*$/\1/g')
+
+if [ ! -z "$2" ];
+then CITY=$2
+fi
+
+
+CITY_URL=$(w3m -dump http://fil.nrk.no/yr/viktigestader/verda.txt | grep -i $'\t'"$CITY"$'\t' | sed -e 's/^.*varsel\.xml//g')
+if [ -z "$CITY_URL" ];
+then exit 1
+fi
+
+TEMP=$(w3m -dump $CITY_URL | grep temperature | head -n $LINE | tail -1 | sed -e 's/^.*value.*"\(.*\)".*$/\1/g')
 echo ${TEMP}
